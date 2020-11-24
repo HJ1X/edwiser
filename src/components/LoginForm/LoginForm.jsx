e3cea5b87631e99d105c7406966ec04000d9afd4
HJ1X <54548986+HJ1X@users.noreply.github.com>
2020-11-24 23:32:37 +0530

    style login and signup form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,24 +1,36 @@
 import React, {useState} from 'react';
 import { Link } from "react-router-dom";
+import { Button, TextField, makeStyles } from "@material-ui/core";
 import './LoginForm.css';
 import TabNav from '../TabNav/TabNav';
 
+const useStyles = makeStyles({
+    root: {
+        display: 'block',
+        marginTop: 20,
+        borderRadius: 20
+    }
+})
+
+const textFieldStyles = {
+    padding: 100
+}
+
 function LoginForm() {
     const [selected, setSelected] = useState('student');
+    const classes = useStyles();
 
     return (
         <div className="LoginForm">
             <TabNav setSelected={setSelected} />
-            <div className="MentorLogin">
+            <div className="Form">
                 <form action="" method="post">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
-                    <input type="submit" value="Login" />
+                    <TextField fullWidth={true} size="small" className={classes.root} variant="outlined" name="username" type="text" label="Username" />
+                    <TextField fullWidth={true} size="small" className={classes.root} variant="outlined" name="password" type="password" label="Password" />
+                    <Button className={classes.root} variant="contained" >Login</Button>
                 </form>
             </div>
-            <p className="create-account">Not a member yet ? <Link to="/signup">Create an account now</Link>.</p>
+            <p className="create-account">Not a member yet? <Link to="/signup">Create an account now</Link>.</p>
         </div>
     )
 }
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -2,20 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { Button, TextField, makeStyles } from "@material-ui/core";
 import './Signup.css';
 
+const useStyles = makeStyles({
+    root: {
+        display: 'block',
+        marginTop: 20,
+        borderRadius: 20
+    }
+})
+
 function Signup() {
+    const classes = useStyles();
+
     return (
         <div className="Signup">
             <Header />
             <div className="Body">
                 <div className="SignupForm">
                     <form action="" method="post">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" id="username" />
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
-                        <input type="submit" value="Login" />
+                        <TextField fullWidth={true} size="small" className={classes.root} variant="outlined" name="username" type="text" label="Username" />
+                        <TextField fullWidth={true} size="small" className={classes.root} variant="outlined" name="password" type="password" label="Password" />
+                        <TextField fullWidth={true} size="small" className={classes.root} variant="outlined" name="confirm-password" type="password" label="Confirm Password" />
+                        <Button className={classes.root} variant="contained" >Create Account</Button>
                     </form>
                     <p className='login'>Already a member ? <Link to="/login">Login here</Link>.</p>
                 </div>
