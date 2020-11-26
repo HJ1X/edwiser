1c46ad9e9954fdf72df6b13883a22f8063fc1252
HJ1X <54548986+HJ1X@users.noreply.github.com>
2020-11-26 23:39:32 +0530

    add reconnect

---
+++ src/components/Dashboard/Reconnect/Reconnect.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reconnect.css';
+
+function Reconnect() {
+    return (
+        <div className="Reconnect">
+
+        </div>
+    )
+}
+
+export default Reconnect;
